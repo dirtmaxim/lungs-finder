961b49cdc519deeae9a46123056fe38d124c21dd
Maksym Kholiavchenko <m.kholyavchenko@innopolis.ru>
2019-06-01 23:06:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author="Maksym Kholiavchenko",
     author_email="dirtmaxim@gmail.com",
     url="https://github.com/dirtmaxim/lungs-finder",
+    download_url="https://github.com/dirtmaxim/lungs-finder/archive/1.0.0.tar.gz",
     license="Apache-2.0",
     packages=find_packages(),
     package_data={"lungs_finder": ["*.xml", "*.np"]}
